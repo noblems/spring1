903a38a3629f2b97ececf6d7688e129e64ed2663
noble.saji <noble.saji@CPU-1382.speridian.com>
2019-07-12 15:57:31 +0530

    changed to context

--- firstspring/src/com/javatpoint/Test.java
+++ firstspring/src/com/javatpoint/Test.java
@@ -1,16 +1,22 @@
     package com.javatpoint;  
       
     import org.springframework.beans.factory.BeanFactory;  
-    import org.springframework.beans.factory.xml.XmlBeanFactory;  
-    import org.springframework.core.io.ClassPathResource;  
+    import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;  
     import org.springframework.core.io.Resource;  
       
     public class Test {  
     public static void main(String[] args) {  
-        Resource resource=new ClassPathResource("applicationContext.xml");  
-        BeanFactory factory=new XmlBeanFactory(resource);  
-          
+        //Resource resource=new ClassPathResource("applicationContext.xml");  
+        //BeanFactory factory=new XmlBeanFactory(resource);  
+        ApplicationContext factory =   
+                new ClassPathXmlApplicationContext("applicationContext.xml");    
         Student student=(Student)factory.getBean("studentbean");  
-        student.displayInfo();  
+        student.displayInfo(); 
+        System.out.println(student.getName());
+        student=(Student) factory.getBean("stbean");
+        student.displayInfo();
     }  
     }  
